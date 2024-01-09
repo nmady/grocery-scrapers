495589227fc5584d8f328278eeee9fc03294820f
Roshan Shariff <roshan.shariff@gmail.com>
2024-01-08 21:46:21 -0700

    Fix Walmart scraper and add pyproject.toml and poetry.lock

--- walmart-scraper/walmart_scraper.py
+++ walmart-scraper/walmart_scraper.py
@@ -1,79 +1,82 @@
 '''Run as:
-	> python walmart_scraper.py filepath
+        > python walmart_scraper.py filepath
 
-	or even
-	> python walmart_scraper.py filepath --output mycart.csv
-''' 
+        or even
+        > python walmart_scraper.py filepath --output mycart.csv
+'''
 
 import json
 import typer
 import sys
 import csv
+from decimal import Decimal
 from bs4 import BeautifulSoup
 
-def convert_possible_cents(price_string):
-	if price_string[-1] == u'\xa2':
-		encoded = price_string.encode("ascii", "ignore")
-		decoded = encoded.decode()
-		return '0.' + str(decoded).zfill(2)
-	else:
-		return price_string
-	
+def parse_price(price_string):
+        price_string = price_string.removesuffix("/ea").removeprefix("$")
+        if price_string.endswith("¢"):
+                price_string = "0." + price_string.removesuffix("¢").zfill(2)
+        return Decimal(price_string)
 
 def main(filepath: str,
-	output: str = typer.Option(None, help="Path to save output csv file."),
-	debug: bool = typer.Option(False, help="If True, prints the JSON file instead.")):
-	"""
-    Scrape your Voila order from a saved html file at FILEPATH.
-    Visit your cart and save the website an html file.
+         output: str = typer.Option(None, help="Path to save output csv file."),
+         """
+         Scrape your Voila order from a saved html file at FILEPATH.
+         Visit your cart and save the website an html file.
+         """
 
-    """
+        if output is not None:
+                sys.stdout = open(output, "w")
 
-	if output is not None:
-		sys.stdout = open(output, "w")
+        with open(filepath) as fp:
+                soup = BeautifulSoup(fp, 'html5lib')
 
+        writer = csv.DictWriter(sys.stdout, fieldnames=["Quantity","Name","Link","Price Each","Price Subtotal","Savings"])
+        writer.writeheader()
 
-	with open(filepath) as fp:
-	    soup = BeautifulSoup(fp, 'html.parser')
+        cart = soup.find(attrs={"data-testid": "product-tile-container"}).find("ul")
+        
+        for item in cart.children:
 
-	writer = csv.DictWriter(sys.stdout, fieldnames=["Quantity","Name","Link","Price Each","Price Subtotal","CRF","Deposit"])
-	writer.writeheader()
-	
-	cart = soup.find(attrs={"data-automation": "active-cart"})
-	
-	for row in cart.find_all(attrs={"data-automation": "product-item-row"}):
+                linktag = item.find("a", {"link-identifier": "itemClick"})
 
-		item = row.parent
-		
-		quantitytag = item.find(attrs={"data-automation": "quantity"})
-		quantity = int(quantitytag.input.get("value"))
+                eachpricestr = item.find("div", {"data-testid": "productDescription"}).text
+                eachprice = parse_price(eachpricestr)
+                
+                discountedpricetag = item.find(attrs={"data-testid": "item-price"})
+                discountedprice = parse_price(discountedpricetag.string) if discountedpricetag \
+                        else None
 
-		linktag = item.find(attrs={"data-automation": "product-item-title"})
+                actualprice = discountedprice or eachprice
 
-		eachpricetag = (item.find(attrs={"data-automation": "product-each-price-with-savings"}) or
-				item.find(attrs={"data-automation": "product-each-price"}))
+                subtotalstr = item.find("div", {"class": "ml-auto"}).find("span").string
+                subtotal = parse_price(subtotalstr)
+                
+                quantitystepper = item.find(attrs={"data-testid": "quantity-stepper"})
+                quantitytag = quantitystepper.find("button").next_sibling
+                quantity = int(quantitytag.string)
 
-		subtotalpricetag = (item.find(attrs={"data-automation": "product-subtotal-price-with-savings"}) or
-				    item.find(attrs={"data-automation": "product-subtotal-price"}))
+                savings = quantity * (eachprice - actualprice)
+                assert(savings >= 0)
 
-		pricetag = eachpricetag
-		if not pricetag:
-			assert quantity == 1
-			pricetag = subtotalpricetag
+                writer.writerow({
+                        "Quantity": str(quantity),
+                        "Name": linktag.text,
+                        "Link": linktag.get("href"),
+                        "Price Each": actualprice,
+                        "Price Subtotal": subtotal,
+                        "Savings": str(savings) if savings else ""
+                })
 
-		crftag = item.find(attrs={"data-automation": "CRF"})
-		deptag = item.find(attrs={"data-automation": "Bev. Deposit"})
-
-		writer.writerow({
-			"Quantity": str(quantity),
-			"Name": linktag.string,
-			"Link": linktag.get("href"),
-			"Price Each": convert_possible_cents(pricetag.string),
-			"Price Subtotal": convert_possible_cents(subtotalpricetag.string),
-			"CRF": convert_possible_cents(crftag.string) if crftag else "",
-			"Deposit": convert_possible_cents(deptag.string) if deptag else ""
-		})
+        writer.writerow({})
+        for feeitem in soup.find_all(attrs={"data-testid": "fee"}):
+                feetype = feeitem.string
+                amount = parse_price(feeitem.next_sibling.string)
+                writer.writerow({
+                        "Name": feetype,
+                        "Price Subtotal": amount
+                })
 
 
 if __name__ == "__main__":
-    typer.run(main)
+        typer.run(main)

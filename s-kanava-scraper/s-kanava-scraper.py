c02b7bc46586b9797f2a04da919e647babe11ee2
Erik Lintunen <erik.lintunen@aalto.fi>
2024-03-01 17:11:27 +0200

    Modify s-kanava-scraper to scrape multiple receipts

--- s-kanava-scraper/s-kanava-scraper.py
+++ s-kanava-scraper/s-kanava-scraper.py
@@ -1,42 +1,47 @@
+import os
+import sys
+
 import typer
 
-def main(filepath: str,
+def main(directory: str,
     output: str = typer.Option(None, help="Path to save output csv file.")):
     """
-    Filepath is a S-group text receipt; can be copied and pasted from s-kanava.fi
-
+    directory: relative path to the folder of S-group text receipts;
+    each of which can be copied and pasted from s-kanava.fi
     """
 
     if output is not None:
         sys.stdout = open(output, "w")
 
-    with open(filepath) as fp:
-        while True:
+    for x in [f for f in os.listdir(directory) if not f.startswith('.')]:
+        with open(f"{directory}/{x}") as fp:
+            print(x)
+            while True:
 
-            line = fp.readline()
-     
-            if not line:
-                break
+                line = fp.readline()
+        
+                if not line:
+                    break
 
-            if line[0] == ' ':
-                continue
+                if line[0] == ' ':
+                    continue
 
-            # reverse line so as to split on last comma
-            comma_broken = line[::-1].split(',', 1)
-            if len(comma_broken) < 2:
-                # more commas than expected => not an item line
-                continue
-            pre_comma = comma_broken[1]
-            try:
-                cents = int(comma_broken[0][::-1].strip())
-            except(ValueError):
-                # what follows the comma is not an int => not an item line
-                continue
-            euros_rev, item_rev = tuple(pre_comma.split(' ', 1))
-            if item_rev[::-1].strip() == 'YHTEENSÄ':
-                # not keeping the total, nor anything after it
-                break
-            print(item_rev[::-1] + ';' + euros_rev[::-1] + ',' + str(cents))
+                # reverse line so as to split on last comma
+                comma_broken = line[::-1].split(',', 1)
+                if len(comma_broken) < 2:
+                    # more commas than expected => not an item line
+                    continue
+                pre_comma = comma_broken[1]
+                try:
+                    cents = int(comma_broken[0][::-1].strip())
+                except(ValueError):
+                    # what follows the comma is not an int => not an item line
+                    continue
+                euros_rev, item_rev = tuple(pre_comma.split(' ', 1))
+                if item_rev[::-1].strip() == 'YHTEENSÄ':
+                    # not keeping the total, nor anything after it
+                    break
+                print(item_rev[::-1] + ';' + euros_rev[::-1] + ',' + str(cents))
 
 if __name__ == "__main__":
     typer.run(main)
